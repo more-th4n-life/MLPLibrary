64d76b4d66d26ea56634a4861ee9c5dae61e881d
Michael Podbury <imgapodbury@outlook.com>
2022-04-01 16:42:47 +1100

    cleaning up cross entropy loss

--- network/loss.py
+++ network/loss.py
@@ -16,33 +16,31 @@ class CrossEntropyLoss(Loss):
     Calc softmax then negative log-likelihood loss
     """
     def __init__(self):
-        self.cache = None
+        self.prob, self.label = None, None
         self.epsilon = 1e-09
 
-    def softmax(self, x, n):
+    def softmax(self, x):
         """
         Returns probabilities for each class
         """
-        e_out = np.exp(x - np.amax(x, axis=1).reshape(n, 1))
-        return (e_out) / np.sum(e_out, axis=1, keepdims=True)
+        clas = np.exp(x - np.max(x, axis=1, keepdims=True)) 
+        return clas / np.sum(clas, axis=1, keepdims=True)
 
     def forward(self, x, label):
         """
         Uses softmax to calculate Cross entropy loss
         """
-        prob = self.softmax(x, x.shape[0])
-        self.cache = label, prob
-        return -np.mean(np.log(prob + self.epsilon) * label)  # prevent log(0)
+        self.prob, self.label = self.softmax(x), label
+        return -np.mean(np.log(self.prob + self.epsilon) * label)  # prevent log(0)
 
     def backward(self):
         """
         Returns difference between softmax probs and ground truth for update
         """
-        label, prob = self.cache
-        return (prob - label) / prob.shape[0]
+        return (self.prob - self.label) / self.prob.shape[0]
 
-    def __call__(self, input, target):
-        return self.forward(input, target)
+    def __call__(self, x, label):
+        return self.forward(x, label)
 
 if __name__ == "__main__":
     from loader.process import one_hot
